73d24d6db469b0322d544ad74d49b59754da9251
usman <tasayev.usman@gmail.com>
2021-09-13 15:21:24 -0400

    fixed images not loading error

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 const menu = {
   "burger": {
-    img: '../dist/images/burger.jpg',
+    img: 'images/burger.jpg',
     name: 'Loaded Burger',
     price: '$12.99'
   },
   "bonein": {
-    img: '../dist/images/bonein.jpeg',
+    img: 'images/bonein.jpeg',
     name: '8-Piece Tradition Wings',
     price: '$7.99'
   },
   "boneout": {
-    img: '../dist/images/boneless.jpeg',
+    img: 'images/boneless.jpeg',
     name: '8-Piece Boneless Wings',
     price: '$7.99'
   },
   "fries": {
-    img: '../dist/images/french-fries.jpg',
+    img: 'images/french-fries.jpg',
     name: 'Fries',
     price: '$4.99'
   }
@@ -24,7 +24,7 @@ const menu = {
 const home = {
   title: 'The Spot',
   description: `The Spot, opened it's doors in 2011, in West springfield, MA. Now it spans over 600 locations across the United States, and Canada. It's founder, xXxFalconSniperDestroyerxXx used to have a favorite restaurant that shut down, it was considered "The Spot" with his boys. That gave him the idea, why not create a spot for everyone? That is when he decided to open his own franchise, naming it "The Spot".`,
-  img: '../dist/images/the-spot.jpg'
+  img: 'images/the-spot.jpg'
 }
 
 
